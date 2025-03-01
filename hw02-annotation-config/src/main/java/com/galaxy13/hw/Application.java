1f35af0c2468070c5ac477bce61e27ef06749457
Galaxy13 <gen.hort54@gmail.com>
2025-03-01 21:43:16 +0300

    2025.03.01: Application annotaions fix

--- hw02-annotation-config/src/main/java/com/galaxy13/hw/Application.java
+++ hw02-annotation-config/src/main/java/com/galaxy13/hw/Application.java
@@ -4,8 +4,10 @@ import com.galaxy13.hw.service.TestRunnerService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
+@Configuration
 @ComponentScan
 @PropertySource("classpath:application.properties")
 public class Application {
