8a3d89bac0c04271e946e32a248fe28e8160f601
Galaxy13 <gen.hort54@gmail.com>
2025-08-11 17:01:22 +0300

    2025.08.11: initial commit

---
+++ hw16-healthcheck/src/main/java/com/galaxy13/hw/dto/CommentDto.java
@@ -0,0 +1,4 @@
+package com.galaxy13.hw.dto;
+
+public record CommentDto(long id, String text, long bookId) {
+}
---
+++ hw16-healthcheck/src/main/java/com/galaxy13/hw/dto/upsert/GenreUpsertDto.java
@@ -0,0 +1,8 @@
+package com.galaxy13.hw.dto.upsert;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record GenreUpsertDto(@NotNull(message = "Id field is mandatory") Long id,
+                             @NotBlank(message = "Genre name can't be empty") String name) {
+}
