109d4602b20a17c285339da282a554cb4bbd3e33
Galaxy13 <gen.hort54@gmail.com>
2025-09-02 17:03:34 +0300

    2025.09.02: indicator fix

--- hw16-healthcheck/src/main/java/com/galaxy13/hw/health/BookHealthIndicator.java
+++ hw16-healthcheck/src/main/java/com/galaxy13/hw/health/BookHealthIndicator.java
@@ -1,25 +1,22 @@
 package com.galaxy13.hw.health;
 
-import com.galaxy13.hw.model.Book;
 import com.galaxy13.hw.repository.BookRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
-
 @Component
 @RequiredArgsConstructor
-public class HealthCheckBookRepository implements HealthIndicator {
+public class BookHealthIndicator implements HealthIndicator {
 
     private final BookRepository bookRepository;
 
     @Override
     public Health health() {
-        List<Book> books = bookRepository.findAll();
-        if (books.isEmpty()) {
-            return Health.unknown()
+        long books = bookRepository.count();
+        if (books == 0) {
+            return Health.down()
                     .withDetail("repository", "Book repository is empty!").build();
         }
         return Health.up().build();
---
+++ hw16-healthcheck/src/test/java/com/galaxy13/hw/health/BookIndicatorTest.java
@@ -0,0 +1,7 @@
+package com.galaxy13.hw.health;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class BookIndicatorTest {
+}
